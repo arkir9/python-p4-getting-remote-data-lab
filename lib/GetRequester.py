c36ca287d20ef063239d3ed61fd43871a5538582
Mohammed <khamismitch@gmail.com>
2024-02-29 09:46:36 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -3,11 +3,17 @@ import json
 
 class GetRequester:
 
-    def __init__(self, url):
+    def __init__(self, url = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"):
         self.url = url
+        
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+     programs_list = []
+     programs = json.loads(self.get_response_body())
+     for program in programs:
+            programs_list.append(program)
+     return programs_list
